f8bb8e831975d0002fdb9bfe133fc6d674ee8c7b
Anupam <anupam12597@gmail.com>
2025-01-15 15:43:21 +0530

    modified somecode

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		 System.out.println("project started");
+		 System.out.println("project ......started");
 		SpringApplication.run(Application.class, args);
 	}
 
